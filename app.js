257f77d54f6d7fae82d367bb7cd5c62eeb1457cc
Maverick <supacode@gmail.com>
2020-07-16 04:40:07 +0100

    Add server start script command

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const compression = require('compression');
 
 dotenv.config({
   path: './config.env',
@@ -23,11 +24,21 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
+if (process.env.NODE_ENV === 'production') app.use(compression());
+
 app.use(helmet());
 
 app.use(cookieParser());
 
 app.use(cors());
+
+// test route
+app.use('/api/v1/test', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'ok',
+  });
+});
 
 app.use('/api/v1/user', authRoutes);
 app.use('/api/v1/contact', contactRoutes);
